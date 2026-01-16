ef088ff2c534b89b5f38dd914ae060ba2a16933c
tuvijith-09 <tuvijithofficial@gmail.com>
2026-01-16 13:26:44 +0530

    homework(DAY-01)

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,9 @@
+class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        int result = a * b;
+
+        System.out.println("Multiplication = " + result);
+    }
+}
---
+++ PrintBestFriendName.java
@@ -0,0 +1,5 @@
+class PrintBestFriendName {
+    public static void main(String[] args) {
+        System.out.println("My best friend's name is Pranav");
+    }
+}
